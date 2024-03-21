bb700c763f695a9776f1f90654ad43b174e511b9
Shaheer Shukur <shaheershukur@gmail.com>
2024-03-22 00:18:50 +0400

    feat: init commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import { EvervaultCard } from "./components/ui/evervault-card";
+
+export default function Home() {
+  return (
+    <div className="flex h-dvh w-dvw flex-col items-center p-4 md:justify-center">
+      <EvervaultCard>
+        <div className="flex w-full flex-col">
+          <span>Hi, I am Shaheer.</span>
+          <span>An enthusiastic engineer 🚀</span>
+        </div>
+      </EvervaultCard>
+    </div>
+  );
+}
