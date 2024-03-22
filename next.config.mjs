5948237cb25111eed30247890cef72749a618c5b
Shaheer Shukur <shaheershukur@gmail.com>
2024-03-22 16:18:25 +0400

    feat: config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  cleanDistDir: true,
+  assetPrefix: 'https://shaheershukur.github.io/shaheershukur',
 };
 
 export default nextConfig;
