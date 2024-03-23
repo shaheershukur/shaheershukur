9b8281fac3247158c45e1298329ec226f54f17f7
Shaheer Shukur <shaheershukur@gmail.com>
2024-03-23 15:50:37 +0400

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.scss";
+import { Analytics } from '@vercel/analytics/next';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.scss';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Shaheer",
+  title: 'Shaheer',
   description: "Shaheer's Portfolio",
 };
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
